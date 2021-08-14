85352cf59292eb27aa3ecfcffa505f4fc6d79fd3
”Hel <heljohansson371368@gmail.com>
2021-08-14 18:02:00 +0500

    Починил не рабочий код

--- Lexplosion/Gui/Windows/MainWindow.xaml.cs
+++ Lexplosion/Gui/Windows/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using Lexplosion.Global;
 using Lexplosion.Gui.Pages.MW;
 using Lexplosion.Gui.UserControls;
 
@@ -22,10 +25,8 @@ namespace Lexplosion.Gui.Windows
         public int curseforgeInstanceId;
         public Uri instanceLogoPath;
 
-        public ToggleButton selectedToggleButton;
-        public List<string> instanceTags = new List<string>();
-
         private Dictionary<string, Page> Pages = new Dictionary<string, Page>();
+        public Page SelectedPage;
 
         public MainWindow()
         {
@@ -33,7 +34,17 @@ namespace Lexplosion.Gui.Windows
             MainWindow.Obj = this;
 
             MouseDown += delegate { try { DragMove(); } catch { } };
-            this.PagesController<InstanceContainerPage>("MainFrame", this.MainFrame);
+            
+            SelectedPage = InstanceContainerPage.obj;
+            InitializeLeftMenu();
+        }
+
+        private void InitializeLeftMenu() 
+        {
+            LeftPanel leftPanel = new LeftPanel(SelectedPage, LeftPanel.PageType.InstanceContainer, this);
+            Grid.SetColumn(leftPanel, 0);
+            MainColumns.Children.Add(leftPanel);
+            this.PagesController<InstanceContainerPage>("InstanceContainerPage", this.RightFrame);
         }
 
         public void PagesController<T>(string page, Frame frame) where T : Page
