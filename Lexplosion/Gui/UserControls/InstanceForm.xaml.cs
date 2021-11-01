b4179839a367fe6096517a4f7000ba48777ab531
Hel Johansson <nightworldoffical@gmail.com>
2021-11-01 20:35:43 +0500

    коммит

--- Lexplosion/Gui/UserControls/InstanceForm.xaml.cs
+++ Lexplosion/Gui/UserControls/InstanceForm.xaml.cs
@@ -349,6 +349,7 @@ namespace Lexplosion.Gui.UserControls
                 );
                 ManageLogic.ComplitedDownload += InstanceDownloadCompleted;
                 ManageLogic.UpdateInstance(instanceId);
+
             }
         }
 
