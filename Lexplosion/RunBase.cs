c875632b515f5cfb6f1736893e87f0022745879e
Hel Johansson <nightworldoffical@gmail.com>
2022-03-06 13:19:04 +0500

    зайти не могу, зато обновил лого, и переверстал authpage

---
+++ Lexplosion/RunBase.cs
@@ -0,0 +1,34 @@
+﻿namespace Lexplosion
+{
+    internal static class RunBase
+    {
+
+        public static StopTask TaskRun(ThreadStart ThreadFunc)
+        {
+            threads.Wait();
+
+            int key = threads.Add(null);
+
+            var thread = new Thread(delegate ()
+            {
+                int threadKey = key;
+
+                ThreadFunc();
+
+                threads.RemoveAt(threadKey);
+            });
+
+            threads[key] = thread;
+
+            thread.Start();
+            threads.Release();
+
+            return delegate ()
+            {
+                thread.Abort();
+                int threadKey = key;
+                threads.RemoveAt(threadKey);
+            };
+        }
+    }
+}
