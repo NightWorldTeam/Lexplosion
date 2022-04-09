65be332de3d33770e533f1917d4bb022367e5ef9
Tosol <nightworldoffical@gmail.com>
2022-04-10 01:33:08 +0300

    Насрал коммитами блять

--- Lexplosion/Logic/FileSystem/WithDirectory.cs
+++ Lexplosion/Logic/FileSystem/WithDirectory.cs
@@ -1368,8 +1368,8 @@ namespace Lexplosion.Logic.FileSystem
                 }
             }
 
-            //Directory.Delete(tempDir, true);
-            //Directory.Delete(temp, true);
+            Directory.Delete(tempDir, true);
+            Directory.Delete(temp, true);
 
             if (downloadedLibs.Count - startDownloadedLibsCount == updateList.Libraries.Count)
             {
