32afd5195261020ac2b9e5858731e19a5898a6b6
nightworldoffical <nightworldoffical@gmail.com>
2022-06-02 23:32:01 +0300

    adsfghj

--- Lexplosion/Logic/Management/ManageLogic.cs
+++ Lexplosion/Logic/Management/ManageLogic.cs
@@ -48,56 +48,5 @@ namespace Lexplosion.Logic.Management
                 return AuthCode.NoConnect;
             }
         }
-
-        //public static ImportResult ImportInstance(string zipFile, out List<string> errors, ProgressHandlerCallback ProgressHandler)
-        //{ // TODO : этот метод полная хуйня блять, надо доделать, может даже переделать
-        //    string instanceId;
-        //    ImportResult res = WithDirectory.ImportInstance(zipFile, out errors, out instanceId);
-        //    LocalInstance instance = new LocalInstance(instanceId);
-
-        //    InstanceInit result = instance.Check(out string gameVersion); // TODO: тут вовзращать ошибки
-
-        //    if (result == InstanceInit.Successful)
-        //    {
-        //        string javaPath;
-        //        using (JavaChecker javaCheck = new JavaChecker(gameVersion))
-        //        {
-        //            if (javaCheck.Check(out JavaChecker.CheckResult checkResult, out JavaVersion javaVersion))
-        //            {
-        //                bool downloadResult = javaCheck.Update(delegate (int percent)
-        //                {
-        //                    ProgressHandler(DownloadStageTypes.Java, 0, 0, percent);
-        //                });
-
-        //                if (!downloadResult)
-        //                {
-        //                    return ImportResult.JavaDownloadError;
-        //                }
-        //            }
-
-        //            if (checkResult == JavaChecker.CheckResult.Successful)
-        //            {
-        //                javaPath = WithDirectory.DirectoryPath + "/java/" + javaVersion.JavaName + javaVersion.ExecutableFile;
-        //            }
-        //            else
-        //            {
-        //                return ImportResult.JavaDownloadError;
-        //            }
-        //        }
-
-        //        instance.Update(javaPath, ProgressHandler);
-        //    }
-
-        //    // TODO: Тут вырезал строку
-        //    /*
-        //    if (Gui.PageType.Right.Menu.InstanceContainerPage.obj != null)
-        //    {
-        //        Uri logoPath = new Uri("pack://application:,,,/assets/images/icons/non_image.png");
-        //        Gui.PageType.Right.Menu.InstanceContainerPage.obj.BuildInstanceForm(instanceId, UserData.InstancesList.Count - 1, logoPath, UserData.InstancesList[instanceId].Name, "NightWorld", "test", new List<string>());
-        //    }
-        //    */
-
-        //    return res;
-        //}
     }
 }
