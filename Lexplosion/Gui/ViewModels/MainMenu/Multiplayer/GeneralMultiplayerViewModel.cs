743c687b7ea66e1df392a28f78c2d3d992529444
Hel Johannson <nightworldoffical@gmail.com>
2022-07-30 14:34:57 +0500

    Добавил кнопки вкл/выкл, перезапуск сетевой игры

--- Lexplosion/Gui/ViewModels/MainMenu/Multiplayer/GeneralMultiplayerViewModel.cs
+++ Lexplosion/Gui/ViewModels/MainMenu/Multiplayer/GeneralMultiplayerViewModel.cs
@@ -8,6 +8,22 @@ namespace Lexplosion.Gui.ViewModels.MainMenu.Multiplayer
 {
     public class GeneralMultiplayerViewModel : VMBase
     {
+        private RelayCommand _multiplayerOff;
+        public RelayCommand MultiplayerOff 
+        {
+            get => _multiplayerOff ?? new RelayCommand(obj => 
+            {
+                
+            });
+        }
 
+        private RelayCommand _multiplayerRefresh;
+        public RelayCommand MultiplayerRefresh
+        {
+            get => _multiplayerRefresh ?? new RelayCommand(obj =>
+            {
+
+            });
+        }
     }
 }
