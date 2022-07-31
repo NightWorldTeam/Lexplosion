571cef6a62d792ea2861769edb34b85dbbb611bc
Tosol <nightworldoffical@gmail.com>
2022-07-31 22:17:47 +0500

    Сделал анимацию для блюра

--- Lexplosion/Gui/Views/CustomControls/InstanceForm.xaml.cs
+++ Lexplosion/Gui/Views/CustomControls/InstanceForm.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Effects;
 
 namespace Lexplosion.Gui.Views.CustomControls
@@ -15,16 +17,24 @@ namespace Lexplosion.Gui.Views.CustomControls
             InitializeComponent();
         }
 
+        private const double AnimationTime = 200;
+
         private void InstanceLogo_MouseEnter(object sender, MouseEventArgs e) 
         {
             InstanceLogo_Background.Effect = new BlurEffect();
+            InstanceLogo_Background.BeginAnimation(Border.OpacityProperty, new DoubleAnimation(1, 0.5, TimeSpan.FromMilliseconds(AnimationTime)));
+            InstanceLogo_Background.Effect.BeginAnimation(BlurEffect.RadiusProperty, new DoubleAnimation(0, 5, TimeSpan.FromMilliseconds(AnimationTime)));
+            InstanceLogo_Text.BeginAnimation(TextBlock.OpacityProperty, new DoubleAnimation(0, 1, TimeSpan.FromMilliseconds(AnimationTime)));
             InstanceLogo_Text.Visibility = Visibility.Visible;
         }
 
         private void InstanceLogo_MouseLeave(object sender, MouseEventArgs e)
         {
-            InstanceLogo_Background.Effect = null;
-            InstanceLogo_Text.Visibility = Visibility.Collapsed;
+            InstanceLogo_Background.BeginAnimation(Border.OpacityProperty, new DoubleAnimation(0.5, 1, TimeSpan.FromMilliseconds(AnimationTime)));
+            InstanceLogo_Background.Effect.BeginAnimation(BlurEffect.RadiusProperty, new DoubleAnimation(5, 0, TimeSpan.FromMilliseconds(AnimationTime)));
+            InstanceLogo_Text.BeginAnimation(TextBlock.OpacityProperty, new DoubleAnimation(1, 0, TimeSpan.FromMilliseconds(AnimationTime)));
+            //InstanceLogo_Background.Effect = null;
+            //InstanceLogo_Text.Visibility = Visibility.Collapsed;
         }
     }
 }
