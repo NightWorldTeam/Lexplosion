89ebdc70ab2728178bebafb4c6a70729f99da13d
Hel Johannson <nightworldoffical@gmail.com>
2022-07-31 18:51:41 +0500

    Починил loadingboard

--- Lexplosion/Controls/LoadingBoard/LoadingBoard.cs
+++ Lexplosion/Controls/LoadingBoard/LoadingBoard.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Lexplosion.Controls
 {
@@ -13,11 +14,17 @@ namespace Lexplosion.Controls
     {
         #region Properties and Events
 
-        public static readonly DependencyProperty IsLoadingFinishedProperty =
-            DependencyProperty.Register("IsLoadingFinished", typeof(bool), typeof(LoadingBoard), new PropertyMetadata(false));
+        public static readonly DependencyProperty IsLoadingFinishedProperty 
+            = DependencyProperty.Register("IsLoadingFinished", typeof(bool), typeof(LoadingBoard), new PropertyMetadata(false));
 
-        public static readonly DependencyProperty PlaceholderProperty =
-            DependencyProperty.Register("Placeholder", typeof(string), typeof(LoadingBoard), new PropertyMetadata(string.Empty));
+        public static readonly DependencyProperty PlaceholderProperty 
+            = DependencyProperty.Register("Placeholder", typeof(string), typeof(LoadingBoard), new PropertyMetadata(string.Empty));
+
+        public static readonly DependencyProperty RectangeColorProperty 
+            = DependencyProperty.Register("RectangeColor", typeof(Brush), typeof(LoadingBoard), new PropertyMetadata(Brushes.White));
+
+        public static readonly DependencyProperty BorderColorProperty
+            = DependencyProperty.Register("BorderColor", typeof(Brush), typeof(LoadingBoard), new PropertyMetadata(Brushes.White));
 
         public bool IsLoadingFinished
         {
@@ -29,6 +36,18 @@ namespace Lexplosion.Controls
         {
             get => (string)GetValue(PlaceholderProperty);
             set => SetValue(PlaceholderProperty, value);
+        }
+
+        public Brush RectangeColor
+        {
+            get => (Brush)GetValue(RectangeColorProperty);
+            set => SetValue(RectangeColorProperty, value);
+        }
+
+        public Brush BorderColor
+        {
+            get => (Brush)GetValue(BorderColorProperty);
+            set => SetValue(BorderColorProperty, value);
         }
 
         #endregion
