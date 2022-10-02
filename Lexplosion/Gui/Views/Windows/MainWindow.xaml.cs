0648b2d37c60947ee0d79b1780a09178c26a5499
Hel Johannson <nightworldoffical@gmail.com>
2022-10-02 14:56:16 +0500

    Установил библиотеку для tray

--- Lexplosion/Gui/Views/Windows/MainWindow.xaml.cs
+++ Lexplosion/Gui/Views/Windows/MainWindow.xaml.cs
@@ -22,69 +22,6 @@ namespace Lexplosion.Gui.Views.Windows
         {
             InitializeComponent();
             MouseDown += delegate { try { DragMove(); } catch { } };
-
-            notifyIcon = new System.Windows.Forms.NotifyIcon();
-            notifyIcon.Icon = new System.Drawing.Icon(App.GetResourceStream(new Uri("pack://application:,,,/Assets/images/icons/logo.ico")).Stream);
-            notifyIcon.Visible = true;
-            notifyIcon.Text = "Lexplosion";
-
-            _trayMenu = (Popup)this.TryFindResource("TTrayMenu");
-            _trayMenu.Opened += _trayMenu_Opened;
-            _trayMenu.Closed += _trayMenu_Closed;
-
-            notifyIcon.Click += NofityIcon_Click;
-            Runtime.ExitEvent += LauncherClosedHandler;
-            Runtime.TrayMenuElementClicked += () => { _trayMenu.IsOpen = false; _isTrayButtonClicked = true; };
-        }
-
-        private void _trayMenu_Closed(object sender, EventArgs e)
-        {
-            MouseHelper.Stop();
-            MouseHelper.MouseLeftClickedEvent -= MouseHelper_MouseLeftClickedEvent;
-        }
-
-        private void _trayMenu_Opened(object sender, EventArgs e)
-        {
-            _isTrayButtonClicked = false;
-            MouseHelper.Start();
-            MouseHelper.MouseLeftClickedEvent += MouseHelper_MouseLeftClickedEvent;
-        }
-
-        private void MouseHelper_MouseLeftClickedEvent(int x, int y)
-        {
-            var coords = NativeMethods.GetControlCoordinate(_trayMenu.Child);
-            if (IsPointInControl(coords[0], coords[1], coords[2], coords[3], x, y)) 
-            {
-                if (_isTrayButtonClicked)
-                    return;
-                else
-                    _trayMenu.IsOpen = true;
-            }
-            else _trayMenu.IsOpen = false;
-        }
-
-        private void LauncherClosedHandler()
-        {
-            notifyIcon.Dispose();
-        }
-
-        private void NofityIcon_Click(object sender, EventArgs e)
-        {
-            var mouseEventArgs = e as System.Windows.Forms.MouseEventArgs;
-            if (mouseEventArgs.Button == MouseButtons.Left)
-            {
-                Runtime.ShowApp();
-                
-            }
-            else if (mouseEventArgs.Button == MouseButtons.Right) 
-            {
-                _trayMenu.IsOpen = true;
-            }
-        }
-
-        private bool IsPointInControl(int left, int top, int right, int bottom, int x, int y) 
-        {
-            return left <= x && right >= x && top <= y && bottom >= y;
         }
 
         private void ChangeStatusButtonClick(object sender, RoutedEventArgs e)
