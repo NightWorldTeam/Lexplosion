7b1459ea759bcfcdd38200e9584a7d26c114cadb
Hel Johannson <nightworldoffical@gmail.com>
2022-11-15 22:10:56 +0500

    Онимация для педиков

--- Lexplosion/Gui/Views/Pages/MainMenu/GeneralSettingsView.xaml.cs
+++ Lexplosion/Gui/Views/Pages/MainMenu/GeneralSettingsView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
 
 namespace Lexplosion.Gui.Views.Pages.MainMenu
 {
@@ -10,6 +13,15 @@ namespace Lexplosion.Gui.Views.Pages.MainMenu
         public GeneralSettingsView()
         {
             InitializeComponent();
+            this.Opacity = 0.0;
+            this.Visibility = Visibility.Visible;
+            DoubleAnimation doubleAnimation = new DoubleAnimation()
+            {
+                From = 0.0,
+                To = 1.0,
+                Duration = TimeSpan.FromSeconds(0.4)
+            };
+            this.BeginAnimation(FrameworkElement.OpacityProperty, doubleAnimation);
         }
     }
 }
