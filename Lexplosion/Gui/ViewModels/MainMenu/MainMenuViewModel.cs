f2219611f7a5dbc33babaf6aeec9cdee563ebc29
nightworldoffical <nightworldoffical@gmail.com>
2022-12-05 21:19:04 +0300

    установли нормальную ссылку

--- Lexplosion/Gui/ViewModels/MainMenu/MainMenuViewModel.cs
+++ Lexplosion/Gui/ViewModels/MainMenu/MainMenuViewModel.cs
@@ -76,7 +76,7 @@ namespace Lexplosion.Gui.ViewModels.MainMenu
             _multiplayerTabMenu = new TabMenuViewModel(_multiplayerTabs, ResourceGetter.GetString("multiplayer"));
             if (GlobalData.User.AccountType == AccountType.NightWorld)
             {
-                _multiplayerTabMenu.ShowButton("Как играть по сети", () => Process.Start("https://night-world.org/faq"));
+                _multiplayerTabMenu.ShowButton("Как играть по сети", () => Process.Start("https://night-world.org/faq#onlineGame"));
             }
 
 
