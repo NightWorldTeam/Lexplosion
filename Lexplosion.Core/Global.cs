634da0941c1dfa8930621817a80b584ee688db25
Sklaip <SvyatoslavShmelkov@yandex.ru>
2023-05-10 23:39:07 +0500

    проделана огромная работа

--- Lexplosion.Core/Global.cs
+++ Lexplosion.Core/Global.cs
@@ -43,9 +43,9 @@ namespace Lexplosion.Global
         }
 
         public const string GAME_FOLDER_NAME = "lexplosion";
-
         public static string LauncherDataPath = Environment.ExpandEnvironmentVariables("%appdata%") + "/lexplosion-data";
         public static string gamePath = Environment.ExpandEnvironmentVariables("%appdata%") + "/." + GAME_FOLDER_NAME;
+
         public const string secretWord = "iDRCQxDMwGVCjWVe0ZEJ4u9DeG38BNL52x777trQ";
         public const string passwordKey = "ZEmMJ0ZaXQXuHu8tUnfdaCLCQaFgRjOP";
         public const int version = 1683481344;
