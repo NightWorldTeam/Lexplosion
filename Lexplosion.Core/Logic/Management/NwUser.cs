b49adf8239a9c8fc179007cb0f49c286b8280fee
Sklaip <SvyatoslavShmelkov@yandex.ru>
2023-05-22 22:27:34 +0300

    это немного, но это честная работа

---
+++ Lexplosion.Core/Logic/Management/NwUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lexplosion.Logic.Management
+{
+    public class NwUser
+    {
+        public string Login { get; private set; }
+
+        public ActivityStatus Status { get; private set; }
+
+        public void SenFriendRequest()
+        {
+
+        }
+    }
+}
