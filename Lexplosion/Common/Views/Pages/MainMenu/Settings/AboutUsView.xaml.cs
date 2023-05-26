9fa36af39133ffa9958684edfe53f1eaf7a51674
Hel Johansson <igor.shaposhnikov@urfu.me>
2023-05-26 11:33:51 +0500

    Переработал немного страницу о лаунчере. Сделал нормальные стрелочки в пагинаторе FindFriends

--- Lexplosion/Common/Views/Pages/MainMenu/Settings/AboutUsView.xaml.cs
+++ Lexplosion/Common/Views/Pages/MainMenu/Settings/AboutUsView.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Reflection;
+﻿using Lexplosion.Global;
+using Lexplosion.Logic.Network;
+using System.Reflection;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace Lexplosion.Common.Views.Pages.MainMenu.Settings
 {
@@ -11,7 +15,22 @@ namespace Lexplosion.Common.Views.Pages.MainMenu.Settings
         public AboutUsView()
         {
             InitializeComponent();
+
+            CopyrightTextBlock.Text = GlobalData.GeneralSettings.LanguageId == "ru-RU" ? "© NightWorld, 2022г. Все права защищены." : "Copyright © 2022 NightWorld. All rights reserved.";
+            ProtectionTextBlock.Text = GlobalData.GeneralSettings.LanguageId == "ru-RU" ? "Данная программа защищена законами об авторских правах и международными соглашениями. Незаконное воспроизведение или распространение данной программы или любой ее части влечет гражданскую и уголовкную ответственность." : "This program is protected by copyright laws and international treaties. Illegal reproduction or distribution of this software or any part of it is subject to civil and criminal liability.";
+
             VersionTextBlock.Text = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            var hel2xBitmap = new BitmapImage();
+            hel2xBitmap.BeginInit();
+            hel2xBitmap.UriSource = new System.Uri("https://night-world.org/requestProcessing/getUserImage.php?user_login=_Hel2x_");
+            hel2xBitmap.EndInit();
+            Hel2xHead.Background = new ImageBrush(hel2xBitmap);
+
+            var sklaipBitmap = new BitmapImage();
+            sklaipBitmap.BeginInit();
+            sklaipBitmap.UriSource = new System.Uri("https://night-world.org/requestProcessing/getUserImage.php?user_login=Sklaip");
+            sklaipBitmap.EndInit();
+            SklaipHead.Background = new ImageBrush(sklaipBitmap);
         }
     }
 }
