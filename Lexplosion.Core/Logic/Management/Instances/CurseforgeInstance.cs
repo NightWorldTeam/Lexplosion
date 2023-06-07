d69e2cfb591560b08b3a98dcfb695e0cb9b1c61e
Sklaip <SvyatoslavShmelkov@yandex.ru>
2023-06-07 13:01:01 +0300

    Вырубаем ебанный курсфордж

--- Lexplosion.Core/Logic/Management/Instances/CurseforgeInstance.cs
+++ Lexplosion.Core/Logic/Management/Instances/CurseforgeInstance.cs
@@ -14,6 +14,8 @@ namespace Lexplosion.Logic.Management.Instances
     {
         public override bool CheckUpdates(InstancePlatformData infoData, string localId)
         {
+            return false;
+
             var content = DataFilesManager.GetFile<InstanceContentFile>(WithDirectory.DirectoryPath + "/instances/" + localId + "/instanceContent.json");
             if (content != null && !content.FullClient)
             {
