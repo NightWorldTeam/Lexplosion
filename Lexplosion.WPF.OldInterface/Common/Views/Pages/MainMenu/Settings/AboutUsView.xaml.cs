7ea415fcf4d24ba2e662098cfc55d104f67d6ff4
Шапошников Игорь <igor.shaposhnikov@urfu.me>
2023-08-18 13:27:27 +0500

    Отказ от ответственности

--- Lexplosion.WPF.OldInterface/Common/Views/Pages/MainMenu/Settings/AboutUsView.xaml.cs
+++ Lexplosion.WPF.OldInterface/Common/Views/Pages/MainMenu/Settings/AboutUsView.xaml.cs
@@ -19,6 +19,7 @@ namespace Lexplosion.Common.Views.Pages.MainMenu.Settings
 
             CopyrightTextBlock.Text = GlobalData.GeneralSettings.LanguageId == "ru-RU" ? "© NightWorld, 2022г. Все права защищены." : "Copyright © 2022 NightWorld. All rights reserved.";
             ProtectionTextBlock.Text = GlobalData.GeneralSettings.LanguageId == "ru-RU" ? "Данная программа защищена законами об авторских правах и международными соглашениями. Незаконное воспроизведение или распространение данной программы или любой ее части влечет гражданскую и уголовную ответственность." : "This program is protected by copyright laws and international treaties. Illegal reproduction or distribution of this software or any part of it is subject to civil and criminal liability.";
+            DenialOfResponsibilityTextBlock.Text = GlobalData.GeneralSettings.LanguageId == "ru-RU" ? "НЕ ЯВЛЯЕТСЯ ОФИЦИАЛЬНЫМ [ПРОДУКТОМ/УСЛУГОЙ/СОБЫТИЕМ И т. п.] MINECRAFT. НЕ ОДОБРЕНО И НЕ СВЯЗАНО С КОМПАНИЕЙ MOJANG ИЛИ MICROSOFT" : "NOT AN OFFICIAL MINECRAFT [PRODUCT/SERVICE/EVENT/etc.]. NOT APPROVED BY OR ASSOCIATED WITH MOJANG OR MICROSOFT";
 
             VersionTextBlock.Text = Assembly.GetExecutingAssembly().GetName().Version.ToString();
             var hel2xBitmap = new BitmapImage();
