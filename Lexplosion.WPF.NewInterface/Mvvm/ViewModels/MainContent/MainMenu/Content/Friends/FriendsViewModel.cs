0371d4427ac79e36767a9e34add2cadb58c58fd1
Hel Johansson <igor.shaposhnikov@urfu.me>
2023-12-09 00:37:54 +0500

    Добавил возможность динамически выводить друзей

--- Lexplosion.WPF.NewInterface/Mvvm/ViewModels/MainContent/MainMenu/Content/Friends/FriendsViewModel.cs
+++ Lexplosion.WPF.NewInterface/Mvvm/ViewModels/MainContent/MainMenu/Content/Friends/FriendsViewModel.cs
@@ -1,17 +1,22 @@
 ﻿using Lexplosion.WPF.NewInterface.Core;
+using Lexplosion.WPF.NewInterface.Mvvm.Models.MainContent.MainMenu.Friends;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace Lexplosion.WPF.NewInterface.Mvvm.ViewModels.MainContent.MainMenu
 {
-    public struct Friend
-    {
-
-    }
-
     public class FriendsViewModel : ViewModelBase
     {
         private ObservableCollection<Friend> _friends = new ObservableCollection<Friend>();
         public IEnumerable<Friend> Friends { get => _friends; }
+
+
+        public FriendsViewModel()
+        {
+            for (var i = 0; i < 3; i++) 
+            {
+                _friends.Add(new Friend($"Person {i}", "Online", (Friend.FriendState)i));
+            }
+        }
     }
 }
