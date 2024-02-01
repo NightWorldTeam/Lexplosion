0bf3655a68342fea1ca4d94efbee307117b8f261
Hel Johansson <igor.shaposhnikov@urfu.me>
2024-02-01 19:42:49 +0500

    Сделал возможность использовать converters без создания объекта в xaml

---
+++ Lexplosion.WPF.NewInterface/Core/Converters/ConverterBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Lexplosion.WPF.NewInterface.Core.Converters
+{
+    public abstract class ConverterBase<T> : MarkupExtension, IValueConverter where T : class, new()
+    {
+        private static T _instance;
+
+
+        #region Constructors
+
+
+        static ConverterBase()
+        {
+            ConverterBase<T>._instance = Activator.CreateInstance<T>();
+        }
+
+        protected ConverterBase()
+        {
+        }
+
+
+        #endregion Constructors
+
+
+        #region Public & Protected Methods
+
+
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (ConverterBase<T>._instance == null) 
+            {
+                ConverterBase<T>._instance = Activator.CreateInstance<T>();
+            }
+            return ConverterBase<T>._instance;
+        }
+
+
+        #endregion Public & Protected Methods
+    }
+}
--- Lexplosion.WPF.NewInterface/Core/Converters/NegativeBooleanToVisibilityConverter.cs
+++ Lexplosion.WPF.NewInterface/Core/Converters/NegativeBooleanToVisibilityConverter.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Globalization;
 using System.Windows;
-using System.Windows.Data;
 
 namespace Lexplosion.WPF.NewInterface.Core.Converters
 {
-    public class NegativeBooleanToVisibilityConverter : IValueConverter
+    public class NegativeBooleanToVisibilityConverter : ConverterBase<NegativeBooleanToVisibilityConverter>
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var bValue = false;
             if (value is bool)
@@ -20,11 +19,6 @@ namespace Lexplosion.WPF.NewInterface.Core.Converters
                 bValue = tmp.HasValue ? tmp.Value : false;
             }
             return (bValue) ? Visibility.Collapsed : Visibility.Visible;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
         }
     }
 }
