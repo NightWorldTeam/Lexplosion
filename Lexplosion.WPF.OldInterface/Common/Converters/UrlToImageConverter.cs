d652439d2a925d41b21cd4a8c64d66c4c5be949b
Hel Johansson <igor.shaposhnikov@urfu.me>
2024-02-15 12:23:14 +0500

    Добавил async конвертор для картинок

--- Lexplosion.WPF.OldInterface/Common/Converters/UrlToImageConverter.cs
+++ Lexplosion.WPF.OldInterface/Common/Converters/UrlToImageConverter.cs
@@ -1,20 +1,26 @@
 ﻿using Lexplosion.Tools;
 using System;
+using System.ComponentModel;
 using System.Globalization;
+using System.Threading.Tasks;
 using System.Windows.Data;
 using System.Windows.Markup;
+using System.Windows.Media;
 
 namespace Lexplosion.Common.Converters
 {
-    internal class UrlToImageConverter : MarkupExtension, IValueConverter
+    internal class UrlToImageConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is string)
             {
-                var url = (string)value;
+                var url = value as string;
 
-                return ImageTools.GetImageByUrl(url);
+                return new AsyncTask(() => 
+                {
+                    return ImageTools.GetImageByUrl(url);
+                });
             }
             return null;
         }
@@ -24,9 +30,27 @@ namespace Lexplosion.Common.Converters
             throw new NotImplementedException();
         }
 
-        public override object ProvideValue(IServiceProvider serviceProvider)
+        public class AsyncTask : INotifyPropertyChanged
         {
-            return this;
+            public AsyncTask(Func<object> valueFunc)
+            {
+                AsyncValue = "loading async value";
+                LoadValue(valueFunc);
+            }
+
+            private async Task LoadValue(Func<object> valueFunc)
+            {
+                AsyncValue = await Task<object>.Run(() =>
+                {
+                    return valueFunc();
+                });
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("AsyncValue"));
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public object AsyncValue { get; set; }
         }
     }
 }
