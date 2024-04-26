3e806c980dee4ab63bf3ab7fba5bd8bfda52e7d0
Igor <igor.shaposhnikov@urfu.me>
2024-04-26 13:17:48 +0500

    Convertors для работы с изображение и null

---
+++ Lexplosion.WPF.NewInterface/Core/Converters/ValueConverters/ImageSourceNullValidateConverter.cs
@@ -0,0 +1,40 @@
+﻿using Lexplosion.WPF.NewInterface.Core.Tools;
+using System;
+using System.Globalization;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Lexplosion.WPF.NewInterface.Core.Converters
+{
+    /// <summary>
+    /// Переводит объект в ImageSource.
+    /// Если объект пустой или NULL, создаёт значение со стандратным изображение.
+    /// </summary>
+    public sealed class ImageSourceNullValidateConverter : ConverterBase<ImageSourceNullValidateConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ImageSource resBitmapImage = ImageTools.defaultBitmapImage;
+            if (value != null) 
+            {
+                if (value is byte[])
+                {
+                    var bytes = value as byte[];
+                    resBitmapImage = ImageTools.ToImageWithoutValidate(bytes);
+                }
+                else if (value is string)
+                {
+                    var str = value as string;
+                    if (str.Length != 0)
+                        resBitmapImage = new BitmapImage(new Uri(str));
+                }
+                else if (value is ImageSource) 
+                {
+                    resBitmapImage = value as ImageSource;
+                }
+            }
+
+            return new ImageBrush(resBitmapImage);
+        }
+    }
+}
--- Lexplosion.WPF.NewInterface/Tools/ImageTools.cs
+++ Lexplosion.WPF.NewInterface/Tools/ImageTools.cs
@@ -1,16 +1,35 @@
 ﻿using System;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace Lexplosion.WPF.NewInterface.Core.Tools
 {
     public static class ImageTools
     {
-        public static BitmapImage ToImage(byte[] imageBytes)
-        {
-            if (imageBytes == null || imageBytes.Length == 0)
-                return new BitmapImage(new Uri("pack://application:,,,/assets/images/icons/non_image.png"));
+        const string pathToDefaultImage = "pack://Application:,,,/Assets/images/icons/non_image.png";
+        public static readonly BitmapImage defaultBitmapImage = new BitmapImage(new Uri(pathToDefaultImage));
 
-            using (var stream = new System.IO.MemoryStream(imageBytes))
+        /// <summary>
+        /// Приводит массив байт в bitmapimage.
+        /// </summary>
+        /// <param name="bytes">Массив байт изображения</param>
+        /// <returns>Итоговое изображение</returns>
+        public static BitmapImage ToImage(byte[] bytes)
+        {
+            if (bytes == null || bytes.Length == 0)
+                return defaultBitmapImage;
+
+            return ToImageWithoutValidate(bytes);
+        }
+
+        /// <summary>
+        /// Приводит массив байт в bitmapimage, не производит проверок на nullю
+        /// </summary>
+        /// <param name="bytes">Массив байт изображения</param>
+        /// <returns>Итоговое изображение</returns>
+        public static BitmapImage ToImageWithoutValidate(byte[] bytes) 
+        {
+            using (var stream = new System.IO.MemoryStream(bytes))
             {
                 var image = new BitmapImage();
                 image.BeginInit();
