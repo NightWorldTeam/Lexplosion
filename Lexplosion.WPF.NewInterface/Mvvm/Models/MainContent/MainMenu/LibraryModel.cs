3c6979ae4d77df0d80054f5cec695183ba494193
Igor <igor.shaposhnikov@urfu.me>
2024-05-08 14:47:43 +0500

    Добавил searchbox для библиотеки

--- Lexplosion.WPF.NewInterface/Mvvm/Models/MainContent/MainMenu/LibraryModel.cs
+++ Lexplosion.WPF.NewInterface/Mvvm/Models/MainContent/MainMenu/LibraryModel.cs
@@ -2,24 +2,50 @@
 using Lexplosion.WPF.NewInterface.Mvvm.Models.InstanceControllers;
 using Lexplosion.WPF.NewInterface.Mvvm.Models.Mvvm.InstanceModel;
 using System.Collections.Generic;
+using System.Windows.Data;
 
 namespace Lexplosion.WPF.NewInterface.Mvvm.Models.MainContent
 {
     public sealed class LibraryModel : ViewModelBase
     {
-        private readonly IInstanceController _instanceCatalog;
-        public IEnumerable<InstanceModelBase> Instances { get => _instanceCatalog.Instances; }
+        private readonly IInstanceController _instanceController;
+        public IReadOnlyCollection<InstanceModelBase> InstanceList { get => _instanceController.Instances; }
+        public CollectionViewSource InstancesCollectionViewSource { get; } = new();
+
+
+        private string _searchText;
+        public string SearchText 
+        {
+            get => _searchText; set 
+            {
+                _searchText = value;
+                OnSearchTextChanged();
+            }
+        }
 
 
         #region Constructors
 
 
-        public LibraryModel(IInstanceController instanceCatalog)
+        public LibraryModel(IInstanceController instanceController)
         {
-            _instanceCatalog = instanceCatalog;
+            _instanceController = instanceController;
+            InstancesCollectionViewSource.Source = _instanceController.Instances;
         }
 
 
         #endregion Constructors
+
+
+        #region Private Methods
+
+
+        private void OnSearchTextChanged() 
+        {
+            InstancesCollectionViewSource.View.Filter = (m => (m as InstanceModelBase).Name.IndexOf(SearchText, System.StringComparison.InvariantCultureIgnoreCase) > -1);
+        }
+
+
+        #endregion Private Methods
     }
 }
