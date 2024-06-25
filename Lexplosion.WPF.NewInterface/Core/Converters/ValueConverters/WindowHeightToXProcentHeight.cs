36e7c5565f42e947a66087e1d4dbe70cbf0d7aa2
Igor <igor.shaposhnikov@urfu.me>
2024-06-26 00:20:32 +0500

    Избавляемся конвертеров в resourcedictionary

--- Lexplosion.WPF.NewInterface/Core/Converters/ValueConverters/WindowHeightToXProcentHeight.cs
+++ Lexplosion.WPF.NewInterface/Core/Converters/ValueConverters/WindowHeightToXProcentHeight.cs
@@ -7,9 +7,9 @@ namespace Lexplosion.WPF.NewInterface.Core.Converters
     // Height="{
     //  Binding ActualHeight,
     //  RelativeSource={RelativeSource AncestorType={x:Type Window}},
-    //  Converter={StaticResource WindowHeightToXProcentHeight}, ConverterParameter=80
+    //  Converter={converters:WindowHeightToXProcentHeight}, ConverterParameter=80
     // }"
-            
+
     public sealed class WindowHeightToXProcentHeight : ConverterBase<WindowHeightToXProcentHeight>
     {
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
