11f493eece5fb7afff17d35e68a8ba86563bb85f
Sklaip <SvyatoslavShmelkov@yandex.ru>
2024-09-28 17:56:23 +0500

    Фикс бага с поиском на модринфе

--- Lexplosion.Core/Logic/Network/Web/ModrinthApi.cs
+++ Lexplosion.Core/Logic/Network/Web/ModrinthApi.cs
@@ -271,7 +271,7 @@ namespace Lexplosion.Logic.Network.Web
 
             facets += "]";
 
-            url += WebUtility.UrlEncode(facets) + "&offset=" + (searchParams.PageIndex * searchParams.PageSize) + "&limit" + searchParams.PageSize;
+            url += facets + "&offset=" + (searchParams.PageIndex * searchParams.PageSize) + "&limit" + searchParams.PageSize; // было WebUtility.UrlEncode(facets), но почему то модринф тупит, если так делать
 
             if (!string.IsNullOrWhiteSpace(searchParams.SearchFilter))
             {
