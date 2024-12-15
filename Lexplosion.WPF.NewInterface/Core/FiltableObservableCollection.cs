d623318246b49b6ab1988aada4eacf95be31800d
Igor Shaposhnikov <Igor.Shaposhnikov@urfu.me>
2024-12-15 18:18:08 +0500

    FiltableObservableCollection и поиск в списке раздач

---
+++ Lexplosion.WPF.NewInterface/Core/FiltableObservableCollection.cs
@@ -0,0 +1,81 @@
+﻿using Lexplosion.WPF.NewInterface.Core.ViewModel;
+using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+
+namespace Lexplosion.WPF.NewInterface.Core
+{
+    public class FiltableObservableCollection : ObservableObject, IEnumerable
+    {
+        #region Properties
+
+
+        private readonly ObservableCollection<object> _filteredCollection = new ObservableCollection<object>();
+        public ICollection Filtered { get => _filteredCollection; }
+
+
+        private IEnumerable _source;
+        public IEnumerable Source
+        {
+            get => _source; set
+            {
+                _source = value;
+                OnSourceChanged();
+            }
+        }
+
+        private Predicate<object> _filter;
+        public Predicate<object> Filter
+        {
+            get => _filter; set
+            {
+                _filter = value;
+                OnFilterChanged();
+            }
+        }
+
+
+        #endregion Properties
+
+
+        #region Public & Protected Methods
+
+
+        protected virtual void UpdateCollectionWithFilter()
+        {
+            _filteredCollection.Clear();
+
+            foreach (var newItem in Source)
+            {
+                if (Filter == null)
+                {
+                    _filteredCollection.Add(newItem);
+                    continue;
+                }
+
+                if (Filter(newItem))
+                {
+                    _filteredCollection.Add(newItem);
+                }
+            }
+        }
+
+        protected virtual void OnFilterChanged()
+        {
+            UpdateCollectionWithFilter();
+        }
+
+        protected virtual void OnSourceChanged()
+        {
+            UpdateCollectionWithFilter();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return Filtered.GetEnumerator();
+        }
+
+
+        #endregion Public & Protected Methods
+    }
+}
--- Lexplosion.WPF.NewInterface/Mvvm/ViewModels/Modal/InstanceTransfer/InstanceDistributionViewModel.cs
+++ Lexplosion.WPF.NewInterface/Mvvm/ViewModels/Modal/InstanceTransfer/InstanceDistributionViewModel.cs
@@ -4,6 +4,8 @@ using Lexplosion.WPF.NewInterface.Commands;
 using Lexplosion.WPF.NewInterface.Core;
 using Lexplosion.WPF.NewInterface.Core.Objects;
 using Lexplosion.WPF.NewInterface.Mvvm.Models.InstanceControllers;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -15,24 +17,44 @@ namespace Lexplosion.WPF.NewInterface.Mvvm.ViewModels.Modal.InstanceTransfer
         private readonly LibraryController _controller;
         private readonly InstanceSharesController _sharesController;
 
-        public IEnumerable<InstanceDistribution> Distributions { get => _sharesController.AvailableInstanceDistribution; }
+        //public IEnumerable<InstanceDistribution> Distributions { get => _sharesController.AvailableInstanceDistribution; }
+
+        private string _filterText = string.Empty;
+        public string FilterText
+        {
+            get => _filterText; set
+            {
+                _filterText = value;
+                OnPropertyChanged();
+                Distributions.Filter = DistFilter;
+            }
+        }
+
+        public FiltableObservableCollection Distributions { get; set; } = new FiltableObservableCollection();
+
         public bool IsEmpty { get; }
 
-
-        public InstanceDistributionModel(LibraryController controller, InstanceSharesController instanceSharesController) 
+        public InstanceDistributionModel(LibraryController controller, InstanceSharesController instanceSharesController)
         {
             _controller = controller;
             _sharesController = instanceSharesController;
+
             LoadInstanceDistribution();
         }
 
+
+        bool DistFilter(object obj)
+        {
+            var dist = (obj as InstanceDistribution);
+            return dist.Name.IndexOf(FilterText ?? string.Empty, System.StringComparison.InvariantCultureIgnoreCase) > -1;
+        }
 
 
         #region Public Methods
 
         public async void LoadInstanceDistribution()
         {
-            if (Account.ActiveAccount == null) 
+            if (Account.ActiveAccount == null)
             {
                 // Todo: throw exeption
                 return;
@@ -40,7 +62,7 @@ namespace Lexplosion.WPF.NewInterface.Mvvm.ViewModels.Modal.InstanceTransfer
 
             var receivers = await Task.Run(() => FileReceiver.GetDistributors(Account.ActiveAccount.UUID, Account.ActiveAccount.SessionToken));
 
-            foreach (var receiver in receivers) 
+            foreach (var receiver in receivers)
             {
                 if (!_sharesController.Contains(receiver.Id))
                 {
@@ -51,9 +73,12 @@ namespace Lexplosion.WPF.NewInterface.Mvvm.ViewModels.Modal.InstanceTransfer
                         );
                 }
             }
+
+            Distributions.Source = _sharesController.AvailableInstanceDistribution;
+            Distributions.Filter = DistFilter;
         }
 
-        public void Download(InstanceDistribution instanceDistribution) 
+        public void Download(InstanceDistribution instanceDistribution)
         {
             instanceDistribution.Download();
         }
@@ -79,7 +104,7 @@ namespace Lexplosion.WPF.NewInterface.Mvvm.ViewModels.Modal.InstanceTransfer
 
 
         private RelayCommand _downloadInstanceCommand;
-        public ICommand DownloadInstanceCommand 
+        public ICommand DownloadInstanceCommand
         {
             get => RelayCommand.GetCommand<InstanceDistribution>(ref _downloadInstanceCommand, Model.Download);
         }
