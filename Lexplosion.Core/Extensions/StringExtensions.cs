a30354484fb00235fa1542cac889ee6120f76294
Igor Shaposhnikov <Igor.Shaposhnikov@urfu.me>
2025-01-02 18:21:20 +0500

    + string extensions

---
+++ Lexplosion.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Lexplosion.Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string FirstCharToUpper(this string input) => input switch
+        {
+            null => input,
+            "" => input,
+            _ => input[0].ToString().ToUpper() + input.Substring(1)
+        };
+    }
+}
