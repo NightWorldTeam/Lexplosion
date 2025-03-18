d304547e18451486d86d3c0ddd711de69acea782
Sklaip <SvyatoslavShmelkov@yandex.ru>
2025-03-19 00:38:46 +0500

    Загрузка картинок

--- Lexplosion.Core/Logic/Objects/MinecraftServerInstance.cs
+++ Lexplosion.Core/Logic/Objects/MinecraftServerInstance.cs
@@ -3,226 +3,238 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using static Lexplosion.Logic.Objects.Curseforge.CurseforgeProjectInfo;
+using System.Net;
 
 namespace Lexplosion.Logic.Objects
 {
-    public class MinecraftServerInstance : VMBase
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
+	public class MinecraftServerInstance : VMBase
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
 
-        public class Tag
-        {
-            [JsonProperty("name")]
-            public string Name { get; }
-            [JsonProperty("id")]
-            public string Id { get; }
+		public class Tag
+		{
+			[JsonProperty("name")]
+			public string Name { get; }
+			[JsonProperty("id")]
+			public string Id { get; }
 
-            [JsonConstructor]
-            public Tag(string name, string id)
-            {
-                Name = name;
-                Id = id;
-            }
-        }
+			[JsonConstructor]
+			public Tag(string name, string id)
+			{
+				Name = name;
+				Id = id;
+			}
+		}
 
-        public readonly struct Links
-        {
-            [JsonProperty("discord")]
-            public string Discord { get; }
-            [JsonProperty("vk")]
-            public string Vk { get; }
-            [JsonProperty("youtube")]
-            public string Youtube { get; }
-            [JsonProperty("telegram")]
-            public string Telegram { get; }
-            [JsonProperty("website")]
-            public string Website { get; }
-
-
-            [JsonConstructor]
-            public Links(string discord, string vk, string youTube, string telegram, string website)
-            {
-                Discord = discord;
-                Vk = vk;
-                Youtube = youTube;
-                Telegram = telegram;
-                Website = website;
-            }
-        }
-
-        public class ModpackData
-        {
-            /// <summary>
-            /// Нужен только если только сборка из кастомного источника. Указывет id источника. 
-            /// Если курсфордж или модринф, то тут будет null
-            /// </summary>
-            [JsonProperty("sourceId")]
-            public string SourceId;
-
-            /// <summary>
-            /// id модпака
-            /// </summary>
-            [JsonProperty("modpackId")]
-            public string ModpackId;
-
-            /// <summary>
-            /// версия модпака
-            /// </summary>
-            [JsonProperty("version")]
-            public string Version;
-
-            public bool IsValid()
-            {
-                return !string.IsNullOrWhiteSpace(SourceId) && !string.IsNullOrWhiteSpace(ModpackId);
-            }
-        }
+		public readonly struct Links
+		{
+			[JsonProperty("discord")]
+			public string Discord { get; }
+			[JsonProperty("vk")]
+			public string Vk { get; }
+			[JsonProperty("youtube")]
+			public string Youtube { get; }
+			[JsonProperty("telegram")]
+			public string Telegram { get; }
+			[JsonProperty("website")]
+			public string Website { get; }
 
 
-        #region Properties
+			[JsonConstructor]
+			public Links(string discord, string vk, string youTube, string telegram, string website)
+			{
+				Discord = discord;
+				Vk = vk;
+				Youtube = youTube;
+				Telegram = telegram;
+				Website = website;
+			}
+		}
+
+		public class ModpackData
+		{
+			/// <summary>
+			/// Нужен только если только сборка из кастомного источника. Указывет id источника. 
+			/// Если курсфордж или модринф, то тут будет null
+			/// </summary>
+			[JsonProperty("sourceId")]
+			public string SourceId;
+
+			/// <summary>
+			/// id модпака
+			/// </summary>
+			[JsonProperty("modpackId")]
+			public string ModpackId;
+
+			/// <summary>
+			/// версия модпака
+			/// </summary>
+			[JsonProperty("version")]
+			public string Version;
+
+			public bool IsValid()
+			{
+				return !string.IsNullOrWhiteSpace(SourceId) && !string.IsNullOrWhiteSpace(ModpackId);
+			}
+		}
 
 
-        [JsonProperty("address")]
-        public string Address { get; }
+		#region Properties
 
-        [JsonProperty("name")]
-        public string Name { get; }
 
-        [JsonProperty("description")]
-        public string Description { get; }
+		[JsonProperty("address")]
+		public string Address { get; }
 
-        [JsonProperty("id")]
-        public string Id { get; }
+		[JsonProperty("name")]
+		public string Name { get; }
 
-        [JsonProperty("tags")]
-        public List<Tag> Tags { get; }
+		[JsonProperty("description")]
+		public string Description { get; }
 
-        [JsonProperty("gameVersion")]
-        public string GameVersion { get; }
+		[JsonProperty("id")]
+		public string Id { get; }
 
-        [JsonProperty("bgUrl")]
-        public string BgUrl { get; }
+		[JsonProperty("tags")]
+		public List<Tag> Tags { get; }
 
-        [JsonProperty("iconUrl")]
-        public string IconUrl { get; }
+		[JsonProperty("gameVersion")]
+		public string GameVersion { get; }
 
-        [JsonProperty("imagesUrls")]
-        public List<string> ImagesUrls { get; }
+		[JsonProperty("bgUrl")]
+		public string BgUrl { get; }
 
+		[JsonProperty("iconUrl")]
+		public string IconUrl { get; }
+
+		[JsonProperty("imagesUrls")]
+		public List<string> ImagesUrls { get; }
+
+
+		[JsonProperty("socialLinks")]
+		public Links SocialLinks { get; set; }
+
+		[JsonProperty("instanceId")]
+		public string InstanceId { get; }
+		[JsonProperty("instanceName")]
+		public string InstanceName { get; }
+
+		private InstanceSource _instanceSource;
+
+		[JsonProperty("instanceSource")]
+		public InstanceSource InstanceSource
+		{
+			get => _instanceSource;
+			set
+			{
+				if (value == InstanceSource.None)
+				{
+					_instanceSource = InstanceSource.Local;
+					return;
+				}
+
+				_instanceSource = Enum.IsDefined(typeof(InstanceSource), (int)value) ? value : InstanceSource.Local;
+			}
+		}
+
+		/// <summary>
+		/// Если сервер ванильный, тут будет null
+		/// </summary>
+		[JsonProperty("modpackInfo")]
+		public ModpackData ModpackInfo { get; set; }
+
+		// not loaded = -2
+		private int _onlineCount = -2;
 		[JsonIgnore]
-		public List<byte[]> Images { get; }
+		public int OnlineCount
+		{
+			get => _onlineCount; set
+			{
+				_onlineCount = value;
+				IsOnline = _onlineCount > -1;
+				OnPropertyChanged();
+			}
+		}
 
-        [JsonProperty("socialLinks")]
-        public Links SocialLinks { get; set; }
+		private bool _isOnline;
+		[JsonIgnore]
+		public bool IsOnline
+		{
+			get => _isOnline; set
+			{
+				_isOnline = value;
+				OnPropertyChanged();
+			}
+		}
 
-        [JsonProperty("instanceId")]
-        public string InstanceId { get; }
-        [JsonProperty("instanceName")]
-        public string InstanceName { get; }
-
-        private InstanceSource _instanceSource;
-
-        [JsonProperty("instanceSource")]
-        public InstanceSource InstanceSource
-        {
-            get => _instanceSource;
-            set
-            {
-                if (value == InstanceSource.None)
-                {
-                    _instanceSource = InstanceSource.Local;
-                    return;
-                }
-
-                _instanceSource = Enum.IsDefined(typeof(InstanceSource), (int)value) ? value : InstanceSource.Local;
-            }
-        }
-
-        /// <summary>
-        /// Если сервер ванильный, тут будет null
-        /// </summary>
-        [JsonProperty("modpackInfo")]
-        public ModpackData ModpackInfo { get; set; }
-
-        // not loaded = -2
-        private int _onlineCount = -2;
-        [JsonIgnore]
-        public int OnlineCount
-        {
-            get => _onlineCount; set
-            {
-                _onlineCount = value;
-                IsOnline = _onlineCount > -1;
-                OnPropertyChanged();
-            }
-        }
-
-        private bool _isOnline;
-        [JsonIgnore]
-        public bool IsOnline
-        {
-            get => _isOnline; set
-            {
-                _isOnline = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private bool _isBannerLoaded;
-        [JsonIgnore]
-        public bool IsBannerLoaded 
-        { 
-            get => _isBannerLoaded; set 
-            {
-                _isBannerLoaded = value;
-                OnPropertyChanged();
-            }
-        } 
+		private bool _isBannerLoaded;
+		[JsonIgnore]
+		public bool IsBannerLoaded
+		{
+			get => _isBannerLoaded; set
+			{
+				_isBannerLoaded = value;
+				OnPropertyChanged();
+			}
+		}
 
 
-        #endregion Properties
+		#endregion Properties
 
 
-        [JsonConstructor]
-        public MinecraftServerInstance(string address, string name, string description, string id, List<Tag> tags, string gameVersion, string bgUrl, string iconUrl, List<string> imagesUrls,
-            string instanceId, string instanceName, InstanceSource instanceSource)
-        {
-            Address = address;
-            Name = name;
-            Description = description;
-            Id = id;
-            Tags = tags;
-            GameVersion = gameVersion;
-            BgUrl = bgUrl;
-            IconUrl = iconUrl;
-            ImagesUrls = imagesUrls;
-            InstanceId = instanceId;
-            InstanceName = instanceName;
-            InstanceSource = instanceSource;
-        }
+		[JsonConstructor]
+		public MinecraftServerInstance(string address, string name, string description, string id, List<Tag> tags, string gameVersion, string bgUrl, string iconUrl, List<string> imagesUrls,
+			string instanceId, string instanceName, InstanceSource instanceSource)
+		{
+			Address = address;
+			Name = name;
+			Description = description;
+			Id = id;
+			Tags = tags;
+			GameVersion = gameVersion;
+			BgUrl = bgUrl;
+			IconUrl = iconUrl;
+			ImagesUrls = imagesUrls;
+			InstanceId = instanceId;
+			InstanceName = instanceName;
+			InstanceSource = instanceSource;
+		}
 
 
-        public bool IsValid()
-        {
-            return !string.IsNullOrWhiteSpace(Name)
-                && !string.IsNullOrWhiteSpace(Id)
-                && !string.IsNullOrWhiteSpace(Address)
-                && !Address.Contains(" ")
-                && MinecraftVersion.IsValidRelease(GameVersion)
-                && (InstanceSource == InstanceSource.Local || (ModpackInfo != null && ModpackInfo.IsValid()));
-        }
-    }
+		public bool IsValid()
+		{
+			return !string.IsNullOrWhiteSpace(Name)
+				&& !string.IsNullOrWhiteSpace(Id)
+				&& !string.IsNullOrWhiteSpace(Address)
+				&& !Address.Contains(" ")
+				&& MinecraftVersion.IsValidRelease(GameVersion)
+				&& (InstanceSource == InstanceSource.Local || (ModpackInfo != null && ModpackInfo.IsValid()));
+		}
 
-    public class McServerOnlineData
-    {
-        public class PalyersCount
-        {
-            [JsonProperty("online")]
-            public int Online;
-            [JsonProperty("max")]
-            public int Max;
-        }
+		public IEnumerable<byte[]> GetImages()
+		{
+			foreach (string url in ImagesUrls)
+			{
+				using (var webClient = new WebClient())
+				{
+					webClient.Proxy = null;
+					yield return webClient.DownloadData(url);
+				}
+			}
+		}
+	}
 
-        [JsonProperty("players")]
-        public PalyersCount Players;
-    }
+	public class McServerOnlineData
+	{
+		public class PalyersCount
+		{
+			[JsonProperty("online")]
+			public int Online;
+			[JsonProperty("max")]
+			public int Max;
+		}
+
+		[JsonProperty("players")]
+		public PalyersCount Players;
+	}
 }
