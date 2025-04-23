d9828a0be322a71168a78e9db4a446b21ca16c00
BANER <vova.shamin.0503@gmail.com>
2025-04-23 18:03:12 +0300

    Local UA, DE, ZH_CH

--- Lexplosion.WPF.NewInterface/Mvvm/Models/MainContent/MainMenu/GeneralSettings/LanguageSettingsModel.cs
+++ Lexplosion.WPF.NewInterface/Mvvm/Models/MainContent/MainMenu/GeneralSettings/LanguageSettingsModel.cs
@@ -11,9 +11,9 @@ namespace Lexplosion.WPF.NewInterface.Mvvm.Models.MainContent.Content.GeneralSet
 {
     public sealed class LanguageSettingsModel : ViewModelBase
     {
-        public static readonly string[] AvailableLanguages = new string[2]
+        public static readonly string[] AvailableLanguages = new string[5]
         {
-            "ru-RU", "en-US"
+            "ru-RU", "en-US", "uk-UA", "zh-CN", "de-DE"
         };
 
         private LanguageModel _selectedLang;
