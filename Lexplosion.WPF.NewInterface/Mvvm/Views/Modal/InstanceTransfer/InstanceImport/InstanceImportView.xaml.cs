e3d5bf2aac8e74b84287f00ffbfd263cda76942b
Igor Shaposhnikov <Igor.Shaposhnikov@urfu.me>
2025-04-26 19:04:47 +0500

    Автоматический скролл к списку импортируемых сборок при запуске импорта

--- Lexplosion.WPF.NewInterface/Mvvm/Views/Modal/InstanceTransfer/InstanceImport/InstanceImportView.xaml.cs
+++ Lexplosion.WPF.NewInterface/Mvvm/Views/Modal/InstanceTransfer/InstanceImport/InstanceImportView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using Lexplosion.WPF.NewInterface.Mvvm.ViewModels.Modal.InstanceTransfer;
+using System.Runtime.InteropServices;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace Lexplosion.WPF.NewInterface.Mvvm.Views.Modal.InstanceTransfer
@@ -8,9 +10,40 @@ namespace Lexplosion.WPF.NewInterface.Mvvm.Views.Modal.InstanceTransfer
     /// </summary>
     public partial class InstanceImportView : UserControl
     {
+        private InstanceImportViewModel _importViewModel;
+
         public InstanceImportView()
         {
             InitializeComponent();
+            DataContextChanged += OnDataContextChanged;
+        }
+
+        private void OnDataContextChanged(object sender, System.Windows.DependencyPropertyChangedEventArgs e)
+        {
+            //var tmpViewModel = _importViewModel;
+            if (_importViewModel != null) 
+            {
+              _importViewModel.Model.ImportProcesses.CollectionChanged -= ImportProcessesChanged;
+            }
+            _importViewModel = (InstanceImportViewModel)DataContext;
+
+            //if (tmpViewModel != null) 
+            //{
+            //    tmpViewModel.Model.ImportProcesses.CollectionChanged -= ImportProcessesChanged;
+            //}
+
+            if (_importViewModel != null) 
+            {
+                _importViewModel.Model.ImportProcesses.CollectionChanged += ImportProcessesChanged;
+            }
+        }
+
+        private void ImportProcessesChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add) 
+            {
+                PageScroll.ScrollToBottom();
+            }
         }
 
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
@@ -18,5 +51,7 @@ namespace Lexplosion.WPF.NewInterface.Mvvm.Views.Modal.InstanceTransfer
             var border = sender as Border;
             Runtime.DebugWrite($"{border.ActualWidth}x{border.ActualHeight}");
         }
+
+
     }
 }
