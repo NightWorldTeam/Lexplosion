53a9810b79aeb4564f4db96883f7926e07bd7f67
Igor Shaposhnikov <Igor.Shaposhnikov@urfu.me>
2025-06-15 23:44:43 +0500

    Открытие веб-страницы у нескопированных дополнений сборки

--- Lexplosion.WPF.NewInterface/Mvvm/ViewModels/Modal/InstanceFactory/InstanceCopyErrorsViewModel.cs
+++ Lexplosion.WPF.NewInterface/Mvvm/ViewModels/Modal/InstanceFactory/InstanceCopyErrorsViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using Lexplosion.Logic.Management.Addons;
+using Lexplosion.WPF.NewInterface.Commands;
 using Lexplosion.WPF.NewInterface.Core.Modal;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Input;
 
 namespace Lexplosion.WPF.NewInterface.Mvvm.ViewModels.Modal
 {
@@ -8,6 +12,24 @@ namespace Lexplosion.WPF.NewInterface.Mvvm.ViewModels.Modal
     public sealed class InstanceCopyErrorsViewModel : ModalViewModelBase
     {
         public InstanceCopyErrorsModel Model { get; }
+
+
+        private RelayCommand _openExternalResourceCommand;
+        public ICommand OpenExternalResourceCommand
+        {
+            get => RelayCommand.GetCommand<InstanceAddon>(ref _openExternalResourceCommand, (addon) => 
+            {
+                try
+                {
+                    Process.Start(addon.WebsiteUrl);
+                }
+                catch (Exception ex)
+                {
+
+                }
+            });
+        }
+
 
         public InstanceCopyErrorsViewModel(IEnumerable<InstanceAddon> uncopiedAddons)
         {
