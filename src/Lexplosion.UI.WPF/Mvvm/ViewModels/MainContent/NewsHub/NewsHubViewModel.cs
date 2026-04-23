419a4ca6297232ebcf5f879c2f7f66d4637f15dc
Igor Mazurenko <mazurenkoi70@gmail.com>
2026-04-23 18:32:20 +0300

    Доработка дизайна

--- src/Lexplosion.UI.WPF/Mvvm/ViewModels/MainContent/NewsHub/NewsHubViewModel.cs
+++ src/Lexplosion.UI.WPF/Mvvm/ViewModels/MainContent/NewsHub/NewsHubViewModel.cs
@@ -7,11 +7,61 @@ using System.Threading.Tasks;
 
 namespace Lexplosion.UI.WPF.Mvvm.ViewModels.MainContent.NewsHub
 {
+
+    public sealed class NewsItemModel
+    {
+        public string Title { get; }
+        public string Description { get; }
+        public DateTime Date { get; }
+        public string Author { get; }
+
+        public NewsItemModel(string title, string description, DateTime date, string author)
+        {
+            Title = title;
+            Description = description;
+            Date = date;
+            Author = author;
+        }
+    }
+
     public sealed class NewsHubViewModel : ViewModelBase
     {
-        public List<int> Items { get; } = new List<int>()
-            {
-                1,2,3,4,5,6
-            };
+        public List<NewsItemModel> Items { get; }
+
+        public NewsHubViewModel()
+        {
+            Items = new List<NewsItemModel>
+        {
+            new NewsItemModel(
+                "Ивент на ПОДПИСКИ",
+                "24 мая будет конкурс среди игроков...",
+                DateTime.Now,
+                "NightWorld"),
+
+            new NewsItemModel(
+                "Обновление лаунчера",
+                "Мы улучшили производительность...",
+                DateTime.Now.AddDays(-1),
+                "NightWorld"),
+
+            new NewsItemModel(
+                "Новая функция",
+                "Добавлена поддержка модов...",
+                DateTime.Now.AddDays(-2),
+                "NightWorld"),
+
+            new NewsItemModel(
+                "Новая функция",
+                "Добавлена поддержка модов...",
+                DateTime.Now.AddDays(-3),
+                "NightWorld"),
+
+            new NewsItemModel(
+                "Новая функция",
+                "Добавлена поддержка модов...",
+                DateTime.Now.AddDays(-4),
+                "NightWorld"),
+        };
+        }
     }
 }
