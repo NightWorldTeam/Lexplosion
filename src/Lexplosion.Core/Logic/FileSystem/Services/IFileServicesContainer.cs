72d22e1bd4b22c9ccea30cda01aeec9374cff333
ox1s <linamel08v@gmail.com>
2026-06-16 19:45:01 +0300

    fix: добавление контейнера сервиса локализации и прокидывание в AllServices через файловый сервис

--- src/Lexplosion.Core/Logic/FileSystem/Services/IFileServicesContainer.cs
+++ src/Lexplosion.Core/Logic/FileSystem/Services/IFileServicesContainer.cs
@@ -1,9 +1,10 @@
-﻿using Lexplosion.Logic.Network;
+﻿using Lexplosion.Logic.Management.Localization;
+using Lexplosion.Logic.Network;
 using Lexplosion.Logic.Network.Services;
 
 namespace Lexplosion.Logic.FileSystem.Services
 {
-	public interface IFileServicesContainer : IWebServicesContainer
+	public interface IFileServicesContainer : IWebServicesContainer, ILocalizationServicesContainer
 	{
 		public WithDirectory DirectoryService { get; }
 		public DataFilesManager DataFilesService { get; }
